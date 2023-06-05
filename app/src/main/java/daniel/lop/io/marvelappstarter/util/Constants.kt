cbcd609f2d60653fef2066b6c7c0691b55d121cd
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-06-05 16:55:44 -0300

    Criando Extensions

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,11 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+    const val PUBLIC_KEY = "8bc5cbdd1a2949894201f85d742e760a"
+    const val PRIVATE_KEY = "2ea4d85b43be87ad0035f3854337d411bf7d861a"
+
+    const val APIKEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
+}
